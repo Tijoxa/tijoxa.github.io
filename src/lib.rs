a61f41794f7efb5d148dd1f7fe6669664a1a9bc4
Tijoxa <tijoxa@gmail.com>
2025-05-01 21:43:12 +0200

    with wasm

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    // Set the panic hook to provide better error messages in the browser console.
+    utils::set_panic_hook();
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, t!");
+}
